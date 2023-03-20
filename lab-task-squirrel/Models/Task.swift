2cd7b2118819c85e53a2036dbba8388f1298dffd
Tung Dinh <dinhviettung1011@gmail.com>
2023-03-20 18:30:55 -0400

    First commit

---
+++ lab-task-squirrel/Models/Task.swift
@@ -0,0 +1,42 @@
+//
+//  Task.swift
+//  lab-task-squirrel
+//
+//  Created by Charlie Hieger on 11/15/22.
+//
+
+import UIKit
+import CoreLocation
+
+class Task {
+    let title: String
+    let description: String
+    var image: UIImage?
+    var imageLocation: CLLocation?
+    var isComplete: Bool {
+        image != nil
+    }
+
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+
+    func set(_ image: UIImage, with location: CLLocation) {
+        self.image = image
+        self.imageLocation = location
+    }
+}
+
+extension Task {
+    static var mockedTasks: [Task] {
+        return [
+            Task(title: "Your favorite restaurant",
+                 description: "Where do you go to for Friday nights"),
+            Task(title: "Your favorite country",
+                 description: "Which country do you want to visit?"),
+            Task(title: "Your pet",
+                 description: "Show your lovely pet!")
+        ]
+    }
+}
